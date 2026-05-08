4dcc1ad11ed97888d9763a54a291cbc42fef3cca
KrisHenriJ <145380846+KrisHenriJ@users.noreply.github.com>
2026-05-08 14:04:45 +0300

    Uncomment routes for books API

--- routes/books.js
+++ routes/books.js
@@ -1,20 +1,20 @@
-//const express = require('express');
-//const router = express.Router();
-//const data = require('../data/books.json');
+const express = require('express');
+const router = express.Router();
+const data = require('../data/books.json');
 
-//router.get('/', (req, res) =>{
-   // res.status(200).json(data);
-//});
+router.get('/', (req, res) =>{
+    res.status(200).json(data);
+});
 
-//router.get('/:bookId', (req, res) =>{
-    //const requestedBookId = req.params.bookId; // "1"
-  //  const book = data.books.filter(bookInData => { //1
-   //    if(bookInData.id.toString() === requestedBookId) {
-   //         return bookInData;
-  //     }
-  //  }); 
+router.get('/:bookId', (req, res) =>{
+    const requestedBookId = req.params.bookId; // "1"
+    const book = data.books.filter(bookInData => { //1
+       if(bookInData.id.toString() === requestedBookId) {
+            return bookInData;
+       }
+    }); 
     
-  //  res.status(200).json(book);
-//});
+    res.status(200).json(book);
+});
 
-//module.exports = router;
+module.exports = router;
