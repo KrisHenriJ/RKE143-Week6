481fb15fb3fa3777a33c9f6316efb166951e36b7
KrisHenriJ <145380846+KrisHenriJ@users.noreply.github.com>
2026-05-08 14:05:29 +0300

    Refactor server.js to remove commented code

--- server.js
+++ server.js
@@ -1,47 +1,19 @@
-//const express = require("express");
-//const port = 3000;
-//const booksRouter = require("./routes/books");
-
-//const app = express();
-
-//app.set("view engine", "ejs");
-
-//app.get("/", (request, response) => {
-    
-    //response.send("<h1>Hello from app get /<h1>");
-    //response.sendStatus(500);
-    //response.status(500).json({message: "Internal error. Try again laher."});
-    //response.download("server.js");
-    //response.render("index");
-//});
-
-//app.use("/books", booksRouter);
-
-
-//app.listen(port, () => {
-    //console.log(`Server is running on port ${port}.`);
-//});
-
-
-const express = require("express");
+const express = require('express');
 const port = 3000;
-const countryRouter = require("./routes/countries");
+const booksRouter = require('./routes/books');
+const countriesRouter = require('./routes/countries');
 
 const app = express();
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
-app.get("/", (request, response) => {
-    
-    //response.send("<h1>Hello from app get /<h1>");
-    //response.sendStatus(500);
-    //response.status(500).json({message: "Internal error. Try again laher."});
-    //response.download("server.js");
-    response.render("index");
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
-app.use("/country", countryRouter);
+app.use('/books', booksRouter);
+app.use('/countries', countriesRouter);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}.`);
+  console.log(`Server is running port ${port}.`);
 });
